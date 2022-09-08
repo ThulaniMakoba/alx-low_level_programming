f31163f9f3f64acd5b47401d1e4d3b92f19a530a
Thulani Makoba <makhobast@gmail.com>
2022-09-08 16:43:33 -0700

    Betty errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * This is a simple task to show how Puts works
+ *
  * Return: 0 if success.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	/*This is a main code block*/
+	puts("\"Programming is like building a multilingual
+		       puzzle");
 	return (0);
 }
