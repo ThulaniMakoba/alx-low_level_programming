953084fbe243de12c50553e7dcff34ef92e88bed
Thulani Makoba <makhobast@gmail.com>
2022-09-09 13:06:49 -0700

    Prints lower case and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 on Success
+ */
+
+int main(void)
+{
+	char lowerCase;
+	char upperCase;
+
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+	{
+		putchar(lowerCase);
+	}
+
+	for (upperCase = 'A'; upperCase <= 'Z'; upperCase++)
+	{
+		putchar(upperCase);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
