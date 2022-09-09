7819a3e6fe9bbde313cba1a404642de819d81ea0
Thulani Makoba <makhobast@gmail.com>
2022-09-09 14:16:37 -0700

    Two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Print all possible combination of two digits
+ *
+ * Return: Always 0 on Success
+ */
+int main(void)
+{
+	int digit1;
+
+	int intdigit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+			{
+				continue;
+			}
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
